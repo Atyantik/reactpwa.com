bdfacf67f63c35b462b1867f417d2b396ee08a55
Tirth Bodawala <tirthbodawala@atyantik.com>
2017-10-12 13:00:00 +0530

    Remove ssh option from deply.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 yarn build
 cd dist && tar cvfz ../dist.tar.gz . && cd ..
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
 scp dist.tar.gz $ATYANTIK_USER@$ATYANTIK_SERVER:$DEPLOY_PATH
 ssh $ATYANTIK_USER@$ATYANTIK_SERVER "cd $DEPLOY_PATH && tar xvzf dist.tar.gz --overwrite"
 ssh $ATYANTIK_USER@$ATYANTIK_SERVER "cd $DEPLOY_PATH && rm -rf dist.tar.gz"
