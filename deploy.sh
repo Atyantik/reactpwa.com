bf32a42830c585efd5566723fe0b4d94e8b2400c
Tirth Bodawala <tirthbodawala@atyantik.com>
2017-10-12 12:05:52 +0530

    Add deploy.sh and for travis-ci

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+yarn test
+yarn build
+cd dist && tar cvfz ../dist.tar.gz . && cd ..
+sftp $ATYANTIK_USER@$ATYANTIK_SERVER <<< $'put -r ./dist.tar.gz $DEPLOY_PATH'
+ssh $ATYANTIK_USER@$ATYANTIK_SERVER 'cd $DEPLOY_PATH && tar xvzf dist.tar.gz --overwrite'
+ssh $ATYANTIK_USER@$ATYANTIK_SERVER 'cd $DEPLOY_PATH && rm -rf dist.tar.gz'
+ssh $ATYANTIK_USER@$ATYANTIK_SERVER '(which pm2 && ((pm2 restart reactpwa --update-env) || (cd $DEPLOY_PATH && pm2 start server.js --name="reactpwa" -i 3 --watch))) &> /dev/null || echo "OK"'
