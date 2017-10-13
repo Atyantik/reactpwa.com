f68404df1ba50c467bfb528526cc38d533315bc0
Tirth Bodawala <tirthbodawala@atyantik.com>
2017-10-13 19:53:55 +0530

    Remove pm2 restart

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,3 @@ cd dist && tar cvfz ../dist.tar.gz . && cd ..
 scp dist.tar.gz $ATYANTIK_USER@$ATYANTIK_SERVER:$DEPLOY_PATH
 ssh $ATYANTIK_USER@$ATYANTIK_SERVER "cd $DEPLOY_PATH && tar xvzf dist.tar.gz --overwrite"
 ssh $ATYANTIK_USER@$ATYANTIK_SERVER "cd $DEPLOY_PATH && rm -rf dist.tar.gz"
-ssh $ATYANTIK_USER@$ATYANTIK_SERVER "(which pm2 && ((pm2 restart reactpwa --update-env) || (cd $DEPLOY_PATH && pm2 start server.js --name='reactpwa' -i 3 --watch))) &> /dev/null || echo 'OK'"
