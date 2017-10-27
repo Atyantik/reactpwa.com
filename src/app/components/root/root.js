069336731b7a621dd7339dd5d613fc73eec66e00
Tirth Bodawala <tirthbodawala@atyantik.com>
2017-10-27 16:07:02 +0530

    Add google tag manager for managing events

--- src/app/components/root/root.js
+++ src/app/components/root/root.js
@@ -3,6 +3,9 @@ import _ from "lodash";
 import { connect } from "react-redux";
 import { loadStyle } from "../../../core/utils";
 import { screenLoaded } from "../../../core/components/screen/action";
+import { loadScript } from "../../../core/utils/utils";
+
+const __development = process.env.NODE_ENV === "development";
 
 @connect (state => {
   return {
@@ -24,11 +27,24 @@ export default class CoreRoot extends Component {
     return Promise.resolve();
   }
   
+  loadGoogleAnalytics() {
+    if(typeof window === "undefined") return;
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      "gtm.start": new Date().getTime(),
+      event: "gtm.js"
+    });
+    loadScript("https://www.googletagmanager.com/gtm.js?id=GTM-K68ZJZT").catch();
+  }
+  
   componentDidMount() {
     // Trigger screenLoaded once all the preload-css are loaded
     this.loadPreloadCSS().then(() => {
       this.props.dispatch(screenLoaded());
     });
+  
+    // Load google analytics
+    !__development && this.loadGoogleAnalytics();
   }
   render() {
     return this.props.children || null;
