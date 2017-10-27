78385e680bedf47ec893a2ff1f23011f8464b3ea
Tirth Bodawala <tirthbodawala@atyantik.com>
2017-10-27 15:25:19 +0530

    Add browser stack support to website

--- src/app/components/footer/footer.js
+++ src/app/components/footer/footer.js
@@ -1,4 +1,7 @@
 import React, {Component} from "react";
+import Picture from "../../../core/components/picture";
+import BrowserStackLogo from "./BrowserStackLogo.png?sizes=50w+100w+200w";
+import * as styles from "./style.scss";
 
 export default class Header extends Component {
   render() {
@@ -13,6 +16,18 @@ export default class Header extends Component {
               className="text-black"
             >
               Atyantik Technologies Private Limited
+            </a>
+          </div>
+          <hr className="w-100" />
+          <div className="d-block w-100 text-center">
+            <span className="w-100 m-r-2" >Supporters: </span>
+            <a
+              href="https://www.browserstack.com"
+              target="_blank"
+              className="text-black d-inline-block v-middle"
+              rel="nofollow"
+            >
+              <Picture imgClassName={styles["supporter-img"]} image={BrowserStackLogo} alt={"Browser Stack Logo"}/>
             </a>
           </div>
         </div>
