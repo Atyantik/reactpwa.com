fc38f205fe76f9c817e349357d72d4a5cfb57fd7
Yash Thakur <yashthakur@atyantik.com>
2017-10-28 17:42:20 +0530

    Add pwa, offline-access, code-splitting, hot reloading and nextgen js pages to features page.

---
+++ src/app/components/features/layout.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Yash Thakur
+ * Date: 27/10/17
+ * Time: 4:50 PM
+ */
+
+import React, { Component } from "react";
+import Disqus from "../disqus";
+import Transition from "../../../core/components/transition/transition";
+import SidebarLinks from "./sidebar-links";
+import * as styles from "./features.scss";
+
+export default class FeaturesLayout extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col col-md-3 p-2 mt-2">
+          <strong className="text-muted">Navigate to:</strong>
+          <SidebarLinks/>
+          <p>We are busy writing documentation for the boilerplate. Thank you for your patience.</p>
+          <div className="alert alert-warning mt-2">We are looking for contributors and queries to create useful documentation. Please contribute.</div>
+        </div>
+        <div  className="col col-md-9">
+          <Transition
+            sectionName={"docs-content"}
+            className={styles["animator"]}
+            onEnterClassName={styles["fade-in"]}
+            onExitClassName={styles["fade-out"]}
+          >
+            <div className="mt-4">
+              {this.props.children}
+            </div>
+          </Transition>
+          <div className="alert alert-warning mt-5">We are looking for contributors and queries to create useful documentation. Please contribute.</div>
+          <div className="mt-4">
+            <Disqus />
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,10 @@ import { configureRoutes } from "./core/utils/bundler";
 // routes
 import * as Home from "./pages/home";
 import * as Docs from "./pages/docs";
+import * as Features from "./pages/features";
 
 export default configureRoutes([
   Home,
-  Docs
+  Docs,
+  Features
 ]);
