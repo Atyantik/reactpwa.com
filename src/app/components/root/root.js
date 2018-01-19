5f846f8ce0c62969bf2e4ffae5dfb6630e78be12
Tirth Bodawala <tirthbodawala@atyantik.com>
2018-01-19 11:31:45 +0530

    Add chat widget

--- src/app/components/root/root.js
+++ src/app/components/root/root.js
@@ -45,6 +45,21 @@ export default class CoreRoot extends Component {
   
     // Load google analytics
     !__development && this.loadGoogleAnalytics();
+    !__development && this.loadTwakChat();
+  }
+  
+  loadTwakChat() {
+    // eslint-disable-next-line
+    window.Tawk_API= window.Tawk_API||{};
+    window.Tawk_LoadStart =new Date();
+    (function(){
+      const s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src="https://embed.tawk.to/5a6182a0d7591465c706e568/default";
+      s1.charset="UTF-8";
+      s1.setAttribute("crossorigin","*");
+      s0.parentNode.insertBefore(s1,s0);
+    })();
   }
   render() {
     return this.props.children || null;
